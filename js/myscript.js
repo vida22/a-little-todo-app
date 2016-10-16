faa2a06c063db655bfb9716a73e8d70e9df90831
vida22 <Vida@android-26f248891d1edbd7.wv.cc.cmu.edu>
2016-10-16 16:37:55 -0400

    remove hardcoded tasks, placeholder text for cards, and modal reset

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,44 @@
 $(document).ready(function() {
+      //add todo
       $("#add-button").click(addTodo);  
+      //deelte todo
       $(document).on("click",'.material-icons.delete-button', deleteTodo);
+      //show and hide placeholder text for cards
+      checkTodos();
+      $(document).on("click",'#delete-todo-button', checkTodos)
+      //complete todo
       $(document).on("click",'.checkbox', completeTodo);
+      //reset modal
+      $('.modal').on('hidden.bs.modal', resetModal);
     });
+
+//Show/hide placeholder text for cards
+function checkTodos(){
+    //show and hide today's placeholder
+    if ($('#today').children('li').length > 0){
+        console.log("Has todos in Today");
+        $("#todayplaceholder").hide();
+    } else {
+        $("#todayplaceholder").show();
+        console.log("No todos in Today");
+    };
+    //show and hide tomorrow placeholder
+    if ($('#tomorrow').children('li').length > 0){
+        console.log("Has todos in Tomororw");
+        $("#tomorrowplaceholder").hide();
+    } else {
+        $("#tomorrowplaceholder").show();
+        console.log("No todos in Tomorrow");
+    };
+    //show and hide next 7 days placeholder
+    if ($('#next7days').children('li').length > 0){
+        console.log("Has todos in Next 7 days");
+        $("#nextplaceholder").hide();
+    } else {
+        $("#nextplaceholder").show();
+        console.log("No todos in Next 7 days");
+    };
+}
 
 
 //Add Todo
@@ -19,12 +55,13 @@ $(document).ready(function() {
     
       console.log(project_class, due_class);
       
-      $('#addTodoModal').modal('hide');
-
+      //hide modal
       
-        
-      $("#" + due_class).
-      append($('<li>')
+      $('#addTodoModal').modal('hide');
+      
+      //append todo
+      $("#" + due_class)
+      .append($('<li>')
             .append($('<input>').attr("type","checkbox").addClass("checkbox"))
             .append($('<p>').attr("class","myp").text(new_todo))
             .append($('<div>').attr('class','todo-options')
@@ -33,8 +70,19 @@ $(document).ready(function() {
                            .append($('<h3>').addClass('myh3').text(todo_project)))
                     .append($('<i>').addClass("material-icons").addClass("delete-button").text("delete")))
             )
-                             
+      checkTodos(); 
     };
+
+
+//reset modal
+    function resetModal(){
+      console.log("Reset modal");
+      $("#notes").val('');
+      $("#todo-title").val('');
+      $("#project").val('Personal');
+      $("#due").val('Today');
+    }
+
 
 
 //Complete Todo 
